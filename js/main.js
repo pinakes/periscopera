9f2f6c064c5e7a2834e6abdf6408dc08ac1c097b
Francesco Fraioli <fraiolinotes@gmail.com>
2016-02-14 20:24:53 -0500

    json instead of csv

--- js/main.js
+++ js/main.js
@@ -1,34 +1,29 @@
+function getRandomArbitrary(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
 // Settings
-var key = "1e0N4HlljoN53muAk02r3wpxiBUNc7LuZA7Md1ByQpjc",  // key for demo spreadsheet
-    query = "&tqx=out:csv",                       // query returns the first sheet as CSV
-    csvUrl = "https://spreadsheets.google.com/tq?key=" + key + query;  // CORS-enabled server
+function sheetLoaded(data) {
+	data = data.feed.entry.map(function (entry) {
+		return {
+			tags: entry['gsx$tags']['$t'],
+			city: entry['gsx$city']['$t'],
+			country: entry['gsx$country']['$t'],
+			video: entry['gsx$video']['$t']
+		}
+	})
 
-d3.csv(csvUrl, function(error, data) {
-
-    function getRandomArbitrary(min, max) {
-		return Math.floor(Math.random() * (max - min)) + min;
-	}
-
-	var randomData = data[getRandomArbitrary(1, 80)]
-
-	console.log(randomData)
+    var randomData = data[getRandomArbitrary(0, data.length - 1)]
 
 	d3.select(".video")
-		.style("background-image", function() { return "url(" + randomData.video + ")" })
+		.style("background-image", "url(" + randomData.video + ")")
 
 	d3.select(".tags")
-		.text(function() { return randomData.tags })
+		.text(randomData.tags)
 
 	d3.select(".location span")
-		.text(function() { return randomData.city })
+		.text(randomData.city)
 
 	d3.select(".location small")
-		.text(function() { return randomData.country })
-
-	d3.select(".matrix").selectAll(".videos")
-		.data(data)
-		.enter().append("div")
-		.attr("class", "videos").append("img")
-			.attr("class", "video")
-			.attr("src", function(d) { return d.video })
-});
+		.text(randomData.country)
+}
